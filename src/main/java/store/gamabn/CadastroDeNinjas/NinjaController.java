b0735aba101164ea7bf1d644bf47de59f85a450b
Flavio <lanbinhohouse@gmail.com>
2025-06-22 15:01:01 -0300

    Entidades e Persistencia 22/06/2025

--- src/main/java/store/gamabn/CadastroDeNinjas/NinjaController.java
+++ src/main/java/store/gamabn/CadastroDeNinjas/NinjaController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class controller {
+public class NinjaController {
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa e minha primeira mensagem nessa rota";
     }
 }
+
