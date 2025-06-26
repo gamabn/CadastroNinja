a9dfc199adfe9e8ba16e0d47b49d42fe7ff2a9e8
Flavio <lanbinhohouse@gmail.com>
2025-06-26 16:10:05 -0300

    chore: .gitgnore adiciona as minhas credenciaisdo db

--- src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/boasVindas")
+    @GetMapping("/")
     public String boasVindas(){
         return "Essa e minha primeira mensagem nessa rota";
     }
