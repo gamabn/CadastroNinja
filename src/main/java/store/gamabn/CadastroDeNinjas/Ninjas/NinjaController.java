cb43c3e30e4ef1fd3bb53e7916d482089a76fef5
Flavio <lanbinhohouse@gmail.com>
2025-06-29 13:48:20 -0300

    fix: Alterar os nomes das rotas dos Ninjas

--- src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package store.gamabn.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
