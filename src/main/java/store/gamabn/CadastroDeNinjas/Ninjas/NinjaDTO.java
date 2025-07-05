1fda7f2945e69c13faf0de13a71cce1d823eb9bd
Flavio <lanbinhohouse@gmail.com>
2025-07-05 14:19:10 -0300

    Chor: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos e entidades DTO

--- src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,24 @@
 package store.gamabn.CadastroDeNinjas.Ninjas;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import store.gamabn.CadastroDeNinjas.Missoes.MissoesModel;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String imgUrl;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String dificuldade;
+
+
+
 }
--- src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/store/gamabn/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,32 @@
 package store.gamabn.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDto){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDto.getId());
+        ninjaModel.setNome((ninjaDto.getNome()));
+        ninjaModel.setEmail(ninjaDto.getEmail());
+        ninjaModel.setImgUrl(ninjaDto.getImgUrl());
+        ninjaModel.setMissoes(ninjaDto.getMissoes());
+        ninjaModel.setDificuldade(ninjaDto.getDificuldade());
+        return ninjaModel;
+
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDto = new NinjaDTO();
+        ninjaDto.setId(ninjaModel.getId());
+        ninjaDto.setNome(ninjaModel.getNome());
+        ninjaDto.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDto.setIdade(ninjaModel.getIdade());
+        ninjaDto.setDificuldade(ninjaModel.getDificuldade());
+        ninjaDto.setMissoes(ninjaModel.getMissoes());
+        return ninjaDto;
+
+
+    }
 }
